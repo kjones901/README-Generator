d7768d6eaadc8f48800915b0e0d05bc12dfafa9f
Kyle Jones <kyle@Kyles-MacBook-Pro.local>
2023-04-22 11:38:40 -0600

    Updated the question formatting slightly and added a README for the project itself

---
+++ README.md
@@ -0,0 +1,29 @@
+# README Generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  ## Description
+    This application uses prompts within the command line to generate a quality readme.
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credit](#credit)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+      Install node
+  
+  ## Usage 
+    From the README-Generator folder within the command line, run node index.js. Answer the prompts. Your README will be generated in the dist folder.
+  
+  ## Credit
+    Inquirer
+  
+  ## License
+    This project is licensed under the MIT License. This license allows you to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the software without restriction, as long as the original copyright notice and license terms are included. For more information,please see the full text of the MIT License.(https://opensource.org/licenses/MIT).
+  
+  ## Questions
+  - [Github](https://github.com/kjones901)
+  - kjones901@gmail.com
+  
--- utils/questions.js
+++ utils/questions.js
@@ -1,29 +1,28 @@
 /** @format */
 
 const questions = [
-    { type: "input", name: "title", message: "What is your project title?" },
-    { type: "input", name: "description", message: "Describe your project(" },
-    {
-      type: "input",
-      name: "installation",
-      message: "What are the steps for installation?",
-    },
-    { type: "input", name: "usage", message: "How do you use your application" },
-    { type: "input", name: "credit", message: "Give Credit" },
-    {
-      type: "list",
-      name: "license",
-      message: "Select a license",
-      choices: [
-        "No License",
-        "MIT License",
-        "Apache License 2.0",
-        "GNU General Public License 3.0",
-      ],
-    },
-    { type: "input", name: "github", message: "Enter your github username" },
-    { type: "input", name: "email", message: "Enter your email" },
-  ];
-  
-  module.exports = questions;
-  
+  { type: "input", name: "title", message: "What is your project title?" },
+  { type: "input", name: "description", message: "Describe your project:" },
+  {
+    type: "input",
+    name: "installation",
+    message: "What are the steps for installation?",
+  },
+  { type: "input", name: "usage", message: "How do you use your application?" },
+  { type: "input", name: "credit", message: "Give Credit" },
+  {
+    type: "list",
+    name: "license",
+    message: "Select a license:",
+    choices: [
+      "No License",
+      "MIT License",
+      "Apache License 2.0",
+      "GNU General Public License 3.0",
+    ],
+  },
+  { type: "input", name: "github", message: "Enter your github username:" },
+  { type: "input", name: "email", message: "Enter your email:" },
+];
+
+module.exports = questions;
